afdb51f4542253c20cb9db428874e1cd02a1a005
99003649 <78849753+99003649@users.noreply.github.com>
2021-02-24 19:49:28 +0530

    Update User.cs

--- DemoKartBL/User.cs
+++ DemoKartBL/User.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace DemoKartBL
 {
-    class User
+     public class User
     {
+         string user1 = "user";
+         string pass1 = "pass";
+        public int LoginCheck(string user, string pass)
+        {        
+            if(user1 == user && pass1 ==pass)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }         
+        }      
     }
 }
