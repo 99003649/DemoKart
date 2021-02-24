330d1d67a51f6c2c5d0a045f7eba24b2e89cabe0
99003649 <78849753+99003649@users.noreply.github.com>
2021-02-24 19:46:59 +0530

    Update Product.cs

--- DemoKartBL/Product.cs
+++ DemoKartBL/Product.cs
@@ -7,6 +7,27 @@ using System.Threading.Tasks;
 namespace DemoKartBL
 {
     class Product
-    {
+    {       
+            public int Id;
+            public string Name;
+            public int Price;
+            public int Quantity;   
+
+
+            public Product()
+            {
+                Id = 0;
+                Name = null;
+                Price = 0;
+                Quantity = 0;
+            }
+
+            public Product(int Id, string Name, int Price, int Quantity)
+            {
+                this.Id = Id;
+                this.Name = Name;
+                this.Price = Price;
+                this.Quantity = Quantity;
+            }        
     }
 }
