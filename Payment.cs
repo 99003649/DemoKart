44fde889b50785155f64b4ef68aad4783ff45d04
99003614 <78959013+riya25gupta@users.noreply.github.com>
2021-02-25 10:28:51 +0530

    Add files via upload

---
+++ Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Website
+{
+    class Payment
+    {
+        public enum payment_mode
+        {
+            cash,
+            creditCard,
+            debitCard
+        };
+
+        public payment_mode pay1 = payment_mode.cash;
+        public payment_mode pay2 = payment_mode.creditCard;
+        public payment_mode pay3 = payment_mode.debitCard;
+
+    }
+}
