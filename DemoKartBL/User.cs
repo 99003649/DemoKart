863c32997ee3e85eb168b29a4a6102596d889c81
99003649 <78849753+99003649@users.noreply.github.com>
2021-02-25 21:51:55 +0530

    Update User.cs

--- DemoKartBL/User.cs
+++ DemoKartBL/User.cs
@@ -8,18 +8,25 @@ namespace DemoKartBL
 {
      public class User
     {
-         string user1 = "user";
-         string pass1 = "pass";
+        string user1 = "user";
+        string pass1 = "pass";
+        string user2 = "pat";
+        string pass2 = "andrews";
+        string user3 = "nishant";
+        string pass3 = "hawaliya";
+        string user4 = "riya";
+        string pass4 = "gupta";
         public int LoginCheck(string user, string pass)
-        {        
-            if(user1 == user && pass1 ==pass)
+        {
+            if (user1 == user && pass1 == pass || user2 == user && pass2 == pass || user3 == user && pass3 == pass || user4 == user && pass4 == pass)
             {
                 return 1;
             }
             else
             {
                 return 0;
-            }         
-        }      
+            }
+
+        }
     }
 }
