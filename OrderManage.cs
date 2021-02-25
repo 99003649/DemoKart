06ae9c61536cc9858888251f47b19123b859dfdd
99003614 <78959013+riya25gupta@users.noreply.github.com>
2021-02-25 10:23:34 +0530

    Add files via upload

---
+++ OrderManage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Website
+{
+    class OrderManage
+    {
+        public string customer_name;
+        public string customer_address;
+        public long customer_phone_no;
+
+        public string getInfo(string name, string address, long phone)
+        {
+            customer_name = name;
+            customer_address = address;
+            customer_phone_no = phone;
+
+            return customer_name + "\n" + customer_address + "\n" + customer_phone_no;
+        }
+    }
+}
