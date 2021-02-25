df704359673c5ccd96721e384691e5dea567bd26
99003649 <78849753+99003649@users.noreply.github.com>
2021-02-25 23:21:34 +0530

    Update Program.cs

--- DemoKartMainProgram/Program.cs
+++ DemoKartMainProgram/Program.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DemoKartMainProgram
-{
-   using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,18 +22,15 @@ namespace EcommercePortal
                 string pass = Console.ReadLine();
                 User m = new User();
                 int log = m.LoginCheck(user, pass);
-
-
+               
                 //Object declaration
                 Product PrObj = new Product();
                 OrderManage odrObj = new OrderManage();
                 Cart CatObj = new Cart();
 
-
                 //function creation
                 void Display(int n)
                 {
-
                     if (n == 1)
                     {
                         foreach (Product item in PrObj.Items)
@@ -59,7 +48,6 @@ namespace EcommercePortal
                                 Console.WriteLine("Product ID : " + item.Id + "\n" + "Product Name : " + item.Name + "\n" + "Product Quantity : " + item.Quantity + "\n" + "Product Price : " + item.Quantity);
                             }
                     }
-
                     else if (n == 3)
                     {
                         foreach (Product item in PrObj.Items)
@@ -68,7 +56,6 @@ namespace EcommercePortal
                                 Console.WriteLine("Product ID : " + item.Id + "\n" + "Product Name : " + item.Name + "\n" + "Product Quantity : " + item.Quantity + "\n" + "Product Price : " + item.Quantity);
                             }
                     }
-
                     else if (n == 4)
                     {
                         foreach (Product item in PrObj.Items)
@@ -77,12 +64,9 @@ namespace EcommercePortal
                                 Console.WriteLine("Product ID : " + item.Id + "\n" + "Product Name : " + item.Name + "\n" + "Product Quantity : " + item.Quantity + "\n" + "Product Price : " + item.Quantity);
                             }
                     }
-
                     else
                         Console.WriteLine("Wrong CHOICE!!");
                 }
-
-
                 void ShowCart()
                 {
                     Console.WriteLine("\n Your Cart Items: - \n");
@@ -98,13 +82,11 @@ namespace EcommercePortal
 
                 if (log == 1)
                 {
-
                     Console.WriteLine(" Select which categories do you want to see \n Enter 1 for Elcetronics \n Enter 2 for Clothing \n Enter 3 for Grocery \n Enter 4 for Cosmetics ");
                     int s = Convert.ToInt32(Console.ReadLine());
 
                     if (s == 1)
                     {
-
                         Display(s);
                         Console.WriteLine("Enter how many products do you want to add in Cart");
                         int num = Convert.ToInt32(Console.ReadLine());
@@ -113,10 +95,8 @@ namespace EcommercePortal
                         {
                             Console.WriteLine("Enter Id of product to add in Cart");
                             int id = Convert.ToInt32(Console.ReadLine());
-
                             foreach (Product item in PrObj.Items)
                             {
-
                                 if (id == item.Id)
                                 {
                                     Console.WriteLine("Product id : " + item.Id);
@@ -125,8 +105,6 @@ namespace EcommercePortal
                                     Console.WriteLine("Product Quantity : " + item.Quantity);
                                     Console.WriteLine("\nEnter how many quantity you want\n");
                                     int Quant = Convert.ToInt32(Console.ReadLine());
-
-
                                     if (Quant > item.Quantity)
                                     {
                                         Console.WriteLine("Currently out of stock\n");
@@ -138,21 +116,16 @@ namespace EcommercePortal
                                     }
                                     cost = item.Price * Quant;
                                     Console.WriteLine("amount = " + cost);
-
                                     cost = cost + ((cost * 18) / 100);
                                     Console.WriteLine("GST amount = " + cost);
                                     Product customer_cart = new Product(item.Id, item.Name, cost, Quant, 1, "Electronics");
                                     CatObj.cust_cart.Add(customer_cart);
-
                                 }
                             }
-
                         }
                     }
-
                     else if (s == 2)
                     {
-
                         Display(s);
                         Console.WriteLine("Enter how many products do you want to add in Cart");
                         int num = Convert.ToInt32(Console.ReadLine());
@@ -161,7 +134,6 @@ namespace EcommercePortal
                         {
                             Console.WriteLine("Enter Id of product to add in Cart");
                             int id = Convert.ToInt32(Console.ReadLine());
-
                             foreach (Product item in PrObj.Items)
                             {
                                 if (id == item.Id)
@@ -172,8 +144,6 @@ namespace EcommercePortal
                                     Console.WriteLine("Product Quantity : " + item.Quantity);
                                     Console.WriteLine("\nEnter how many quantity you want\n");
                                     int Quant = Convert.ToInt32(Console.ReadLine());
-
-
                                     if (Quant > item.Quantity)
                                     {
                                         Console.WriteLine("Currently out of stock\n");
@@ -181,25 +151,19 @@ namespace EcommercePortal
                                     else
                                     {
                                         item.Quantity = item.Quantity - Quant;
-
                                     }
                                     cost = item.Price * Quant;
                                     Console.WriteLine("amount = " + cost);
-
                                     cost = cost + ((cost * 7) / 100);
                                     Console.WriteLine("GST amount = " + cost);
                                     Product customer_cart = new Product(item.Id, item.Name, cost, Quant, 2, "Clothing");
                                     CatObj.cust_cart.Add(customer_cart);
-
                                 }
                             }
-
                         }
                     }
-
                     else if (s == 3)
                     {
-
                         Display(s);
                         Console.WriteLine("Enter how many products do you want to add in Cart");
                         int num = Convert.ToInt32(Console.ReadLine());
@@ -208,7 +172,6 @@ namespace EcommercePortal
                         {
                             Console.WriteLine("Enter Id of product to add in Cart");
                             int id = Convert.ToInt32(Console.ReadLine());
-
                             foreach (Product item in PrObj.Items)
                             {
                                 if (id == item.Id)
@@ -219,8 +182,6 @@ namespace EcommercePortal
                                     Console.WriteLine("Product Quantity : " + item.Quantity);
                                     Console.WriteLine("\nEnter how many quantity you want\n");
                                     int Quant = Convert.ToInt32(Console.ReadLine());
-
-
                                     if (Quant > item.Quantity)
                                     {
                                         Console.WriteLine("Currently out of stock\n");
@@ -231,24 +192,18 @@ namespace EcommercePortal
 
                                     }
                                     cost = item.Price * Quant;
-
                                     Console.WriteLine("amount = " + cost);
-
                                     cost = cost + ((cost * 4) / 100);
                                     Console.WriteLine("GST amount = " + cost);
                                     Console.WriteLine("amount = " + cost);
                                     Product customer_cart = new Product(item.Id, item.Name, cost, Quant, 3, "Grocery");
                                     CatObj.cust_cart.Add(customer_cart);
-
                                 }
                             }
-
                         }
                     }
-
                     else if (s == 4)
                     {
-
                         Display(s);
                         Console.WriteLine("Enter how many products do you want to add in Cart");
                         int num = Convert.ToInt32(Console.ReadLine());
@@ -268,8 +223,6 @@ namespace EcommercePortal
                                     Console.WriteLine("Product Quantity : " + item.Quantity);
                                     Console.WriteLine("\nEnter how many quantity you want\n");
                                     int Quant = Convert.ToInt32(Console.ReadLine());
-
-
                                     if (Quant > item.Quantity)
                                     {
                                         Console.WriteLine("Currently out of stock\n");
@@ -277,29 +230,22 @@ namespace EcommercePortal
                                     else
                                     {
                                         item.Quantity = item.Quantity - Quant;
-
                                     }
                                     cost = item.Price * Quant;
                                     Console.WriteLine("Product amount is = " + cost);
-
                                     cost = cost + ((cost * 18) / 100);
                                     Console.WriteLine("Product amount after category tax = " + cost);
-
                                     Console.WriteLine("amount = " + cost);
                                     Product customer_cart = new Product(item.Id, item.Name, cost, Quant, 4, "Cosmetics");
                                     CatObj.cust_cart.Add(customer_cart);
-
                                 }
                             }
-
                         }
                     }
                     else
                         Console.WriteLine("INVALID CHOICE!!");
-
                     ShowCart();
                     Console.WriteLine("Your total cart cost with category tax is: -");
-
                     double Tamount = CatObj.TotalCost();
                     Console.WriteLine(Tamount);
                     Console.WriteLine("Enter 1 for order cart items else enter zero: -");
@@ -307,17 +253,13 @@ namespace EcommercePortal
                     double Tprice = 0;
                     double Gprice = 0;
                     if (order == 1)
-
                     {
                         Console.WriteLine("enter reciever's name");
                         string name = Console.ReadLine();
-
                         Console.WriteLine("enter reciever's address");
                         string address = Console.ReadLine();
-
                         Console.WriteLine("enter reciever's phone number");
                         long phone = Convert.ToInt64(Console.ReadLine());
-
                         Console.WriteLine("\n Customer Details \n");
                         Console.WriteLine(odrObj.GetInfo(name, address, phone));
                         Console.WriteLine("SELECT THE PAYMENT MODE");
@@ -339,15 +281,11 @@ namespace EcommercePortal
                             Tprice = Tamount + (Tamount * (4 / 100));
                             Console.WriteLine("Your total pay amount is :" + Tprice);
                         }
-
-
                     }
                     else
                     {
                         Console.WriteLine("Place your order soon, items are limited in stock, Thank you!!");
                     }
-
-
 
                     Console.ReadLine();
                 }
@@ -357,7 +295,7 @@ namespace EcommercePortal
 
                 Console.WriteLine(ex.Message);
             }
-        }
-    }
-}
+         }
+      }
+   }
 }
