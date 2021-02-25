6d50dea155bb886156f88b321bab6efde8bd5de6
99003614 <78959013+riya25gupta@users.noreply.github.com>
2021-02-25 23:24:22 +0530

    Update Payment.cs

--- DemoKartBL/Payment.cs
+++ DemoKartBL/Payment.cs
@@ -1,12 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DemoKartBL
+namespace EcommercePortal
 {
     class Payment
     {
+        public enum payment_mode
+        {
+            cash,
+            creditCard,
+            debitCard
+        };
+
+        public payment_mode pay1 = payment_mode.cash;
+        public payment_mode pay2 = payment_mode.creditCard;
+        public payment_mode pay3 = payment_mode.debitCard;
     }
 }
